05d038a444ab3ff9affa9a4b96d6ae18288b75da
Guilherme Yamasaki <guilhermeyyamasaki1@gmail.com>
2022-06-18 13:27:34 -0300

    Finalizando CRUD de usuários

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,14 @@ const UserController = require('./controllers/UserController')
 //rota para pegar usuários
 routes.get('/users', UserController.index)
 
+//rota para criar usuários
+routes.post('/users', UserController.create)
+
+//rota para atualizar usuários
+routes.put('/users:id', UserController.update)
+
+//rota para deletar usuários
+routes.delete('/users:id', UserController.delete)
+
+
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -3,7 +3,22 @@ const routes = requite('./routes')
 
 const app = express()
 
+app.use(express.json()) //recebe body no estilo json
 app.use(routes)
+
+//not found error
+app.use((req, res, next) => {
+    const error = new Error('Mensagem de erro "not found"')
+    error.status = 404
+    next(error)
+})
+
+//catch all
+//next = próximo passo da requisição
+app.use((req, res, next) => {
+    res.status(error.status || 500)
+    res.json({error: error.message})
+})
 
 
 
